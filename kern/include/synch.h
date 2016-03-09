7aed0a9861ef9c49ef683ba7ceb45cef29adbcb4
Joseph Barnes <jospeh34@gmail.com>
2016-03-09 00:37:29 -0800

    update to synch.h

--- kern/include/synch.h
+++ kern/include/synch.h
@@ -77,6 +77,9 @@ struct lock {
         char *lk_name;
         // add what you need here
         // (don't forget to mark things volatile as needed)
+        struct wchan *lk_wchan;
+        struct spinlock lk_lock;
+        volatile struct thread *lk_holder;
 };
 
 struct lock *lock_create(const char *name);
@@ -116,6 +119,7 @@ struct cv {
         char *cv_name;
         // add what you need here
         // (don't forget to mark things volatile as needed)
+        struct wchan *cv_wchan;
 };
 
 struct cv *cv_create(const char *name);
